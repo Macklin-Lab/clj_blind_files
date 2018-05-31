fcbb99ed47a554920c85441b6bb2c95721c7c953
Nick George <nicholas.george32@gmail.com>
2018-05-30 22:25:33 -0600

    made function to get file names and base path

--- src/clj_blind_files/blind_files.clj
+++ src/clj_blind_files/blind_files.clj
@@ -12,11 +12,12 @@
 ;; I need a few functions that are composable to finish the raw part of the project. The GUI will be separate
 ;; Functions needed:
 ;; 1. DONE +list file names of a given ending+
-;; 2. TODO -get just file names from the full path.-
+;; 2. DONE +get just file names and split the full path.+
 ;; 3. TODO -replace the file name with a uuid random name-
 ;; 4. TODO -make a new folder called "Blinded" in the initial folder.-
 ;; 5. TODO -write old-name, new-name to a KEY.csv in the original folder-
 ;; 6. TODO -write all new-names to a blind-ref.csv in the new folder.-
+;; 7. TODO -GUI-
 
 
 (defn list-files
@@ -25,6 +26,23 @@
   [ending dir]
   (filter #(.endsWith (str %) ending) (mapv str (filter #(.isFile %) (file-seq (io/file dir))))))
 
+(defn name
+  "return file name"
+  [f]
+  (.getName (io/file f)))
+
+(defn parent
+  "return the path"
+  [f]
+  (.getParent (io/file f)))
+
+;; combine parent and child. 
+(defn pathsplitter
+  "returns a list with the path and filename"
+  [f] (let [fileio (io/file f)]
+        (list (.getParent fileio)
+              (.getName fileio))))
+
+(pathsplitter test-file)
 
 
-(map .getName (map io/file (list-files file-end test-dir))) ;; need to do this but failing
